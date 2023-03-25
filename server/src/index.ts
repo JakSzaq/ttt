693b0d548fa8c66180e977ecaedc29575ed5295d
JakSzaq <szafrykj@gmail.com>
2023-03-25 21:58:23 +0100

    Added code to handle realtime client communication

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,3 +11,92 @@ app.get('/', (req, res) => {
 });
 
 const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+
+// socket server
+
+import { Server, Socket } from 'socket.io';
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://127.0.0.1:5173'
+    },
+    connectionStateRecovery: {
+        maxDisconnectionDuration: 2 * 60 * 1000,
+        skipMiddlewares: true,
+    }
+});
+
+const clientsInRoom: object = {};
+const existingRooms: string[] = [];
+
+io.on('connection', (socket: Socket) => {
+    socket.on('reqTurn', (data) => {
+        const room: string = JSON.parse(data).room;
+        io.to(room).emit('playerTurn', data);
+    })
+
+    socket.on('addNewRoom', room => {
+        existingRooms.push(room);
+        console.log(`Existing rooms: ${existingRooms}`)
+    })
+
+    socket.on('createRoom', room => {
+        if (io.sockets.adapter.rooms.has(room) || !existingRooms.includes(room)){
+            socket.emit('error_invalid_room_code'); 
+        } else {
+            socket.join(room);
+            clientsInRoom[room] = io.sockets.adapter.rooms.get(room)?.size;
+            console.log(`New room: ${room}`)
+        }
+    })
+
+    socket.on('joinRoom', room => {
+        if (io.sockets.adapter.rooms.has(room)){
+            clientsInRoom[room] = io.sockets.adapter.rooms.get(room)?.size
+            if (clientsInRoom[room] === 1){
+                io.to(room).emit('opponent_joined_alert');
+                socket.join(room);
+                io.to(room).emit('opponent_joined');
+            } else {
+                socket.emit('error_room_is_full');
+            }
+        } else {
+            socket.emit('error_invalid_room_code'); 
+        }
+    })
+
+    socket.on('reqRestart', (data) => {
+        const room: string = JSON.parse(data).room;
+        io.to(room).emit('restartRoom');
+    })
+
+    socket.on('updateRoom', room => {
+        const players: number | undefined = io.sockets.adapter.rooms.get(room)?.size;
+        if (players === 1){
+            io.to(room).emit('pauseGame');
+        }
+    })
+
+    socket.on("disconnecting", () => {
+        const room: string = updateRooms();
+        io.to(room).emit("gameStatus");
+    });
+
+    socket.on("manualDisconnect", () => {
+        const room: string = updateRooms();
+        socket.leave(room);
+        io.to(room).emit("gameStatus");
+    });
+
+    const updateRooms = () => {
+        const socketRooms: string[] = Array.from(socket.rooms);
+        const room: string = socketRooms[1];
+        if ( io.sockets.adapter.rooms.get(room)?.size === 1 ){
+            const index: number = existingRooms.indexOf(room);
+            if (index > -1) { 
+                existingRooms.splice(index, 1);
+            }
+        }
+        return room;
+    }
+})
